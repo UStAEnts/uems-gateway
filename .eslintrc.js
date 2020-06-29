92629e11f5f4dc3638d2b274f712167db2792454
Ryan Delaney <vitineth@gmail.com>
2020-06-29 22:18:29 +0100

    Adding eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2020: true,
+    },
+    extends: [
+        'airbnb-typescript',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 11,
+        sourceType: 'module',
+        project: './tsconfig.json',
+    },
+    plugins: [
+        '@typescript-eslint',
+    ],
+    rules: {
+        indent: ['error', 4],
+        '@typescript-eslint/naming-convention': [
+            'error',
+            {
+                selector: 'variableLike',
+                format: ['camelCase', 'UPPER_CASE'],
+            },
+        ],
+        '@typescript-eslint/indent': ['error', 4],
+        'import/first': ['off'],
+        'import/order': ['off'],
+        'import/prefer-default-export': ['off'],
+        'max-len': ['error', 120],
+    },
+};
