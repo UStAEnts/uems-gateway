e3f984c142d4455a521aaa330a706c4422c9f589
Paul Lancaster <paul@lancasterzone.com>
2020-07-02 00:12:06 +0100

    Support for specific query? params in get events

--- src/message_handling.ts
+++ src/message_handling.ts
@@ -152,16 +152,15 @@ class GatewayMessageHandler {
 }
 
 function parse_get_event_req_to_message(req) {
-    // TODO, currently returns blank - 'get all' type message.
-    let msg = {
+    return {
         "ID": Math.random() * 100000,
-        "name": "",
-        "start_date_before": "",
-        "start_date_after": "",
-        "end_date_before": "",
-        "end_date_after": "",
+        "name": (req.query.name === undefined) ? "" : req.query.name,
+        "start_date_before": (req.query.start_before === undefined) ? "" : req.query.start_before,
+        "start_date_after": (req.query.start_after === undefined) ? "" : req.query.start_after,
+        "end_date_before": (req.query.end_before === undefined) ? "" : req.query.end_before,
+        "end_date_after": (req.query.end_after === undefined) ? "" : req.query.end_after,
+        "venue": (req.query.venue === undefined) ? "" : req.query.venue
     };
-    return msg;
 }
 
 exports.GatewayMessageHandler = GatewayMessageHandler;
