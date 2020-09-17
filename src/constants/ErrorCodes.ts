dbc770c46323a2f0278689b6eb5a10019ed75bdc
Ryan Delaney <vitineth@gmail.com>
2020-09-17 18:27:54 +0100

    Formalising error codesnnThese should match the format required by the responses so it should would nicely

---
+++ src/constants/ErrorCodes.ts
@@ -0,0 +1,9 @@
+const generate = (code: string, message: string) => ({
+    code,
+    message,
+});
+
+export const ErrorCodes = {
+    FAILED: generate('FAILED', 'An unspecified error occurred in the service, please try again later'),
+    SERVICE_TIMEOUT: generate('SERVICE_TIMEOUT', 'The service did not respond within the maximum allocated time, please try again later'),
+};
