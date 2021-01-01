9e6b8c686a0fc5d72920ce4f5b94cc3d24579c3d
Ryan Delaney <vitineth@gmail.com>
2021-01-01 19:29:28 +0000

    Adding resolving ents states, states and venues for events
    This needs to be redone as venues are an array, not a single entry, will come back to this one

--- src/attachments/attachments/EventGatewayAttachment.ts
+++ src/attachments/attachments/EventGatewayAttachment.ts
@@ -10,6 +10,8 @@ import GatewayAttachmentInterface = GatewayMk2.GatewayAttachmentInterface;
 import SendRequestFunction = GatewayMk2.SendRequestFunction;
 import GatewayInterfaceActionType = GatewayMk2.GatewayInterfaceActionType;
 import RequestCallback = GatewayMk2.RequestCallback;
+import { EntityResolver } from "../../resolver/EntityResolver";
+import { constants } from "http2";
 
 // The topic used for sending get requests to the event details microservice.
 const EVENT_DETAILS_SERVICE_TOPIC_GET: string = 'events.details.get';
@@ -24,8 +26,17 @@ const EVENT_DETAILS_SERVICE_TOPIC_UPDATE: string = 'events.details.update';
 const EVENT_DETAILS_SERVICE_TOPIC_DELETE: string = 'events.details.delete';
 
 export class EventGatewayAttachment implements GatewayAttachmentInterface {
-    async generateInterfaces(send: GatewayMk2.SendRequestFunction): Promise<GatewayInterfaceActionType[]> {
+
+    // TODO: bit dangerous using ! - maybe add null checks?
+    private _resolver!: EntityResolver;
+
+    async generateInterfaces(
+        send: GatewayMk2.SendRequestFunction,
+        resolver: EntityResolver,
+    ): Promise<GatewayInterfaceActionType[]> {
         const validator = await EventResponseValidator.setup();
+
+        this._resolver = resolver;
 
         return [
             {
@@ -37,7 +48,13 @@ export class EventGatewayAttachment implements GatewayAttachmentInterface {
             {
                 action: 'get',
                 path: '/events',
-                handle: EventGatewayAttachment.getEventsHandler(send),
+                handle: this.getEventsHandler(send),
+                additionalValidator: validator,
+            },
+            {
+                action: 'get',
+                path: '/events/:id',
+                handle: this.getEventHandler(send),
                 additionalValidator: validator,
             },
             {
@@ -79,9 +96,11 @@ export class EventGatewayAttachment implements GatewayAttachmentInterface {
                 MessageUtilities.identifierConsumed(reqResponse.msg_id as number);
 
                 if (status === EventRes.MsgStatus.SUCCESS) {
-                    httpRes.status(HttpStatus.NO_CONTENT).send();
+                    httpRes.status(HttpStatus.NO_CONTENT)
+                        .send();
                 } else {
-                    httpRes.status(HttpStatus.NOT_FOUND).json(ErrorCodes.FAILED);
+                    httpRes.status(HttpStatus.NOT_FOUND)
+                        .json(ErrorCodes.FAILED);
                 }
             };
 
@@ -101,7 +120,11 @@ export class EventGatewayAttachment implements GatewayAttachmentInterface {
                 event_id: eventId,
             };
 
-            const { name, startDate, endDate } = req.body;
+            const {
+                name,
+                startDate,
+                endDate
+            } = req.body;
 
             if (name !== undefined) {
                 msg.event_name = name.toString();
@@ -126,17 +149,19 @@ export class EventGatewayAttachment implements GatewayAttachmentInterface {
 
                 MessageUtilities.identifierConsumed(reqResponse.msg_id as number);
                 if (status === EventRes.MsgStatus.SUCCESS) {
-                    httpRes.status(HttpStatus.OK).send({
-                        status: 'OK',
-                        result: {
-                            id: reqResponse.result[0],
-                            name: '',
-                            startDate: 0,
-                            endDate: 0,
-                        },
-                    });
+                    httpRes.status(HttpStatus.OK)
+                        .send({
+                            status: 'OK',
+                            result: {
+                                id: reqResponse.result[0],
+                                name: '',
+                                startDate: 0,
+                                endDate: 0,
+                            },
+                        });
                 } else {
-                    httpRes.status(HttpStatus.NOT_FOUND).send(ErrorCodes.FAILED);
+                    httpRes.status(HttpStatus.NOT_FOUND)
+                        .send(ErrorCodes.FAILED);
                 }
             };
 
@@ -145,7 +170,7 @@ export class EventGatewayAttachment implements GatewayAttachmentInterface {
         };
     }
 
-    private static getEventsHandler(send: SendRequestFunction) {
+    private getEventsHandler = (send: SendRequestFunction) => {
         return async (req: Request, res: Response) => {
             const msg: EventMsg.ReadEventMsg = {
                 msg_id: MessageUtilities.generateMessageIdentifier(),
@@ -189,11 +214,22 @@ export class EventGatewayAttachment implements GatewayAttachmentInterface {
                 MessageUtilities.identifierConsumed(reqResponse.msg_id as number);
                 if (status === EventRes.MsgStatus.SUCCESS) {
                     const result: EventResponse[] = reqResponse.result.map(InternalEventToEventResponse);
-                    httpRes.status(HttpStatus.OK).send(result);
+                    // Now we need to map it twice, not ideal but oh well, TODO: come up with a better way to do this?
+                    this._resolver.resolveEntStateSet(result)
+                        .then(() => this._resolver.resolveStateSet(result))
+                        .then(() => this._resolver.resolveVenueSet(result))
+                        .then(() => {
+                            httpRes.status(HttpStatus.OK)
+                                .send({
+                                    status: 'OK',
+                                    result,
+                                });
+                        });
                 } else {
                     console.log(reqResponse);
                     // Note this return code (503) isn't officially defined yet.
-                    httpRes.status(HttpStatus.SERVICE_UNAVAILABLE).send(ErrorCodes.FAILED);
+                    httpRes.status(HttpStatus.SERVICE_UNAVAILABLE)
+                        .send(ErrorCodes.FAILED);
                 }
             };
 
@@ -202,9 +238,84 @@ export class EventGatewayAttachment implements GatewayAttachmentInterface {
         };
     }
 
+    private getEventHandler(send: SendRequestFunction) {
+        return async (req: Request, res: Response) => {
+            const outgoingMessage: any = {
+                msg_id: MessageUtilities.generateMessageIdentifier(),
+                msg_intention: MsgIntention.READ,
+                status: 0,
+            };
+
+            if (!MessageUtilities.has(req.params, 'id')) {
+                res
+                    .status(constants.HTTP_STATUS_BAD_REQUEST)
+                    .json(MessageUtilities.wrapInFailure({
+                        message: 'missing parameter id',
+                        code: 'BAD_REQUEST_MISSING_PARAM',
+                    }));
+                return;
+            }
+
+            const callback: RequestCallback = (
+                httpRes,
+                timestamp,
+                response,
+                status,
+            ) => {
+                // This is validated by the handler function
+                const reqResponse = response as EventRes.ReadRequestResponseMsg;
+
+                MessageUtilities.identifierConsumed(reqResponse.msg_id as number);
+                if (status === EventRes.MsgStatus.SUCCESS) {
+                    const result: EventResponse[] = reqResponse.result.map(InternalEventToEventResponse);
+                    console.log(result);
+                    if (result.length !== 1){
+                        httpRes.status(HttpStatus.NOT_FOUND)
+                            .send(ErrorCodes.FAILED);
+                        return;
+                    }
+                    // Now we need to map it twice, not ideal but oh well, TODO: come up with a better way to do this?
+                    this._resolver.resolveEntStateSet(result)
+                        .then(() => this._resolver.resolveStateSet(result))
+                        .then(() => this._resolver.resolveVenueSet(result))
+                        .then(() => {
+                            httpRes.status(HttpStatus.OK)
+                                .send({
+                                    status: 'OK',
+                                    result: {
+                                        event: result[0],
+                                        changelog: [],
+                                    },
+                                });
+                        });
+                } else {
+                    console.log(reqResponse);
+                    // Note this return code (503) isn't officially defined yet.
+                    httpRes.status(HttpStatus.SERVICE_UNAVAILABLE)
+                        .send(ErrorCodes.FAILED);
+                }
+            };
+
+            outgoingMessage.event_id = req.params.id;
+
+            console.log(outgoingMessage);
+
+            await send(
+                EVENT_DETAILS_SERVICE_TOPIC_GET,
+                outgoingMessage,
+                res,
+                callback,
+            );
+        };
+    }
+
     private static createEventHandler(send: SendRequestFunction) {
         return async (request: Request, res: Response) => {
-            const { name, startDate, endDate } = request.body;
+            const {
+                name,
+                startDate,
+                endDate
+            } = request.body;
             const msg: EventMsg.CreateEventMsg = {
                 msg_id: MessageUtilities.generateMessageIdentifier(),
                 status: 0, // 0 Code used when the status is still to be decided.
@@ -227,6 +338,7 @@ export class EventGatewayAttachment implements GatewayAttachmentInterface {
 
                 MessageUtilities.identifierConsumed(reqResponse.msg_id as number);
                 if (status === EventRes.MsgStatus.SUCCESS) {
+                    // TODO: the fuck?
                     const result: CreateEventResponse = {
                         status: 'OK',
                         result: {
@@ -234,12 +346,15 @@ export class EventGatewayAttachment implements GatewayAttachmentInterface {
                             name: '',
                             startDate: 0,
                             endDate: 0,
+                            attendance: 0,
                         },
                     };
-                    httpRes.status(HttpStatus.CREATED).send(result);
+                    httpRes.status(HttpStatus.CREATED)
+                        .send(result);
                 } else {
                     // Note this return code (503) isn't officially defined yet.
-                    httpRes.status(HttpStatus.SERVICE_UNAVAILABLE).send(ErrorCodes.FAILED);
+                    httpRes.status(HttpStatus.SERVICE_UNAVAILABLE)
+                        .send(ErrorCodes.FAILED);
                 }
             };
 
