ce4080cceeeaf20f3bdffcb3d2ed97417c3a72dd
Ryan Delaney <vitineth@gmail.com>
2021-01-01 19:27:16 +0000

    Adding temporary types for the API response

--- src/types/GatewayTypes.ts
+++ src/types/GatewayTypes.ts
@@ -1,12 +1,19 @@
 // The gateway types as defined by the external API.
 
 import { EventRes } from '@uems/uemscommlib';
+import { EntStateValidators } from '@uems/uemscommlib/build/ent/EntStateValidators';
+import EntStateRepresentation = EntStateValidators.EntStateRepresentation;
+import { StateValidators } from '@uems/uemscommlib/build/state/StateValidators';
+import StateRepresentation = StateValidators.StateRepresentation;
 
 export type EventResponse = {
     id: string,
     name: string,
     startDate: Number,
-    endDate: Number
+    endDate: Number,
+    attendance: number,
+    ents?: EntStateRepresentation,
+    state?: StateRepresentation,
 };
 
 export type CreateEventResponse = {
@@ -20,5 +27,11 @@ export function InternalEventToEventResponse(ie: EventRes.InternalEvent): EventR
         name: ie.event_name,
         startDate: ie.event_start_date,
         endDate: ie.event_end_date,
+        attendance: ie.attendance,
+
+        // @ts-ignore - TODO: move to comms lib
+        ents: ie.ents,
+        // @ts-ignore - TODO: move to comms lib
+        state: ie.state,
     };
 }
