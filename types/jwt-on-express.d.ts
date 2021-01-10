92221c4b7ccd1e4eafde7de1154783c24fbad8d4
Ryan Delaney <vitineth@gmail.com>
2021-01-10 17:15:38 +0000

    Removing JWT definition on express and providing a better definition

--- types/jwt-on-express.d.ts
+++ types/jwt-on-express.d.ts
@@ -1,12 +1,16 @@
 declare namespace Express {
     interface Request {
-        uemsJWT: {
-            ip: string,
-            userID: string,
-        },
         oidc: {
             isAuthenticated: () => boolean,
-            user: any,
+            user: {
+                /**
+                 * The users ID as delivered by the Auth0 query: REQUIRED. Subject Identifier. A locally unique and
+                 * never reassigned identifier within the Issuer for the End-User, which is intended to be consumed
+                 * by the Client
+                 */
+                sub: string,
+                [key: string]: any,
+            },
         }
     }
 }
