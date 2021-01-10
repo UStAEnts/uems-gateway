8961d13ae4351fd562985864ea12b73bb29de980
Ryan Delaney <vitineth@gmail.com>
2021-01-10 01:10:38 +0000

    Removing app and JWT code, redoing some imports

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,9 @@
 // External dependencies.
-import helmet from 'helmet';
-import express from 'express';
-import logger from 'morgan';
-import * as Cors from 'cors'; // Cors library used to handle CORS on external endpoints.
-import * as BodyParser from 'body-parser'; // Bodyparser used to handle JSON content in messages.
+import fs from 'fs/promises';
 import amqp from 'amqplib';
-import cors from 'cors';
-import jwt, { VerifyOptions } from 'jsonwebtoken';
+import { ZodError } from 'zod';
 
 // Internal dependencies.
-import { UemsAuth } from './UemsAuth';
 import { GatewayMk2 } from './Gateway';
 import { VenueGatewayInterface } from './attachments/attachments/VenueGatewayInterface';
 import { EventGatewayAttachment } from './attachments/attachments/EventGatewayAttachment';
@@ -18,130 +12,15 @@ import { UserGatewayInterface } from './attachments/attachments/UserGatewayInter
 import { EquipmentGatewayInterface } from './attachments/attachments/EquipmentGatewayInterface';
 import { StateGatewayInterface } from './attachments/attachments/StateGatewayInterface';
 import { SystemGatewayInterface } from './attachments/system/SystemGatewayInterface';
-import { TopicGatewayInterface } from "./attachments/attachments/TopicGatewayInterface";
-import { FileGatewayInterface } from "./attachments/attachments/FileGatewayInterface";
-import { SignupGatewayInterface } from "./attachments/attachments/SignupGatewayInterface";
+import { TopicGatewayInterface } from './attachments/attachments/TopicGatewayInterface';
+import { FileGatewayInterface } from './attachments/attachments/FileGatewayInterface';
+import { SignupGatewayInterface } from './attachments/attachments/SignupGatewayInterface';
+import { ExpressApplication, ExpressConfiguration, ExpressConfigurationType } from './express/ExpressApplication';
+import { EntityResolver } from './resolver/EntityResolver';
 import GatewayMessageHandler = GatewayMk2.GatewayMessageHandler;
-import { ExpressApplication, ExpressConfiguration, ExpressConfigurationType } from "./express/ExpressApplication";
-import { EntityResolver } from "./resolver/EntityResolver";
-import { ZodError } from "zod";
 
-const fs = require('fs').promises;
-const passport = require('passport'); // Passport is used for handling external endpoint authentication.
-const AuthStrategy = require('passport-http-bearer').Strategy;
-
-const JWT_PUBLIC_KEY = Buffer.from('2d2d2d2d2d424547494e205055424c4943204b45592d2d2d2d2d0a4d4947624d42414742797147534d343941674547425375424241416a413447474141514150686a724a6f354f6d697337574f3671626f6167435878484348436c0a58485436547a71312f3667737075565450573448716f4278624d534f2f52766d4c7568466c664a44323751506847437377766645504246692b77454177364a590a494f2b70314157384d626d706d2f6e676b61756c47484873643538383154566e6b526a467053785067774b61462f704876325248617a62494d53306e6d5853530a554d50703238484b512f4f652b6970513236733d0a2d2d2d2d2d454e44205055424c4943204b45592d2d2d2d2d0a', 'hex');
 const RABBIT_MQ_CONFIG: string = 'rabbit-mq-config.json';
 const EXPRESS_CONFIG: string = 'express-config.json';
-
-// CORS configuration.
-const corsOptions: Cors.CorsOptions = {
-    origin: '*',
-    methods: 'GET,OPTIONS,PATCH,POST,DELETE',
-    optionsSuccessStatus: 200,
-};
-
-// Authentication configuration.
-passport.use(new AuthStrategy(UemsAuth.authFunction));
-
-const app = express();
-app.use(helmet());
-
-// Enable preflight CORS for all routes.
-// @ts-ignore
-app.options('*', cors(corsOptions));
-app.use(cors(corsOptions));
-
-console.log('Starting uems-gateway...');
-
-app.set('port', process.env.PORT || 15450);
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(BodyParser.json());
-
-// We need to verify that all requests to the gateway have a valid JSON Web Token
-// This token is used to identify that the client has authenticated via the frontend
-// server and that the request should be made on behalf of them. JWTs have the following
-// properties
-// * Data:
-//   * ip
-//   * userID
-// * Settings
-//   * ES512
-//   * Expires in 20 minutes (in production, in development these may be any amount of time)
-//   * Issuer: uems.frontend
-//   * Audience: uems.gateway
-//   * Subject: identifier
-// Public and private keys MUST be regenerated before going into production to invalidate
-// all keys me in development. Keys without expiry should only be used in development and
-// any key that does not expire that is received in production should be rejected and warned
-// Invalidation messages may be received from the frontend server via alternate channels. Any
-// key that is invalidated should be stored and compared until it has reached its expiry time
-// at which point is can be discarded (with a buffer if desired). Invalidations will only be
-// sent when the user logs out. Expired JWTs must be sent with a meaningful response so the client
-// can easily identify that they need to refresh with the server
-// TODO: add invalidated tokens to storage
-const ConditionalJWTVerifyOptions: Partial<VerifyOptions> = process.env.NODE_ENV === 'dev' ? {
-    // If we're in development then we want to accept any token basically regardless of age
-    // this adds some additional protection because if we only use expired tokens in development
-    // they therefore literally cannot be used in production if leaked and someone fucks the keys
-    // even though thats unlikely. Also this saves me having to generate myself keys when they expire,
-    // maybe thats the primary reason, maybe not but I can pretend its for added security
-    ignoreExpiration: true,
-} : {
-    maxAge: '20m',
-};
-
-app.use((req, res, next) => {
-    const header = req.header('X-UEMS-Identifier');
-    if (header === undefined) {
-        res.status(401)
-            .send('authentication token must be included in the request as a header');
-        return;
-    }
-
-    try {
-        const object = jwt.verify(header, JWT_PUBLIC_KEY, {
-            algorithms: ['ES512'],
-            audience: 'uems.gateway',
-            issuer: 'uems.frontend',
-            subject: 'identifier',
-            ...ConditionalJWTVerifyOptions,
-        }) as string | Record<string, any>;
-
-        // If its valid then we want to check that this is valid and it has a user ID and that it is a string and
-        // check that it has an IP and that the ip is valid
-        if (typeof (object) === 'string') throw new Error('invalid token: not object');
-        if (!Object.prototype.hasOwnProperty.call(object, 'userID')) throw new Error('invalid token: no userID');
-        if (!Object.prototype.hasOwnProperty.call(object, 'ip')) throw new Error('invalid token: no ip');
-        if (typeof (object.userID) !== 'string') throw new Error('invalid token: user id is not a string');
-        if (typeof (object.ip) !== 'string') throw new Error('invalid token: ip is not a string');
-        if (process.env.NODE_ENV !== 'dev' && req.ip !== object.ip) throw new Error('invalid token: ip does not match');
-
-        req.uemsJWT = {
-            ip: object.ip,
-            userID: object.userID,
-        };
-
-        next();
-    } catch (e) {
-        if (e.name === 'TokenExpiredError') {
-            res.status(401)
-                .send('authentication token has expired, please refresh');
-            return;
-        }
-
-        res.status(401)
-            .send('invalid authentication token');
-        console.log(e);
-    }
-});
-
-// Event get requests use queries so the query parser must be enabled.
-app.set('query parser', 'extended');
-
 
 async function main() {
     console.log('Attempting to connect to rabbit-mq...');
@@ -244,6 +123,11 @@ async function main() {
     console.log(`Server launched on ${expressValidation.data.port ?? 15450}`);
 }
 
-main();
-
-module.exports = app;
+main()
+    .then(() => {
+        console.log('Launch complete');
+    })
+    .catch((e) => {
+        console.error('Launch failed');
+        console.error(e);
+    });
