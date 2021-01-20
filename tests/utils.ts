207e313b3db6a4c1657119149b5a006dd658cce4
Ryan Delaney <vitineth@gmail.com>
2021-01-20 18:12:52 +0000

    Creating test functions which test for missing wrong type and valid data
    This means that tests should be very easy to write as they all follow the same structure just with different data being passed in.

---
+++ tests/utils.ts
@@ -0,0 +1,91 @@
+import { GatewayMk2 } from "../src/Gateway";
+import GatewayInterfaceActionType = GatewayMk2.GatewayInterfaceActionType;
+import { Response } from "jest-express/lib/response";
+import express from "express";
+import { GET_VENUES_INVALID, request } from "./test-api-data";
+import { constants } from "http2";
+
+export function testParameterTypes(
+    route: GatewayInterfaceActionType,
+    data: any,
+    location: 'query' | 'body',
+    send: jest.Mock,
+    params?: any,
+) {
+    const response = new Response();
+    const fake = response as unknown as express.Response;
+    const req = request(
+        location === 'query' ? data : undefined,
+        location === 'body' ? data : undefined,
+        params,
+    );
+    route.handle(req, fake, () => false);
+
+    if (send.mock.calls.length !== 0) {
+        console.warn(response);
+    }
+    expect(send)
+        .not
+        .toHaveBeenCalled();
+    expect(response.statusCode)
+        .toEqual(constants.HTTP_STATUS_BAD_REQUEST);
+    expect(JSON.stringify(response.body))
+        .toContain('type');
+}
+
+export function testMissingParameters(
+    route: GatewayInterfaceActionType,
+    data: any,
+    location: 'query' | 'body',
+    send: jest.Mock,
+    params?: any,
+) {
+    const response = new Response();
+    const fake = response as unknown as express.Response;
+    const req = request(
+        location === 'query' ? data : undefined,
+        location === 'body' ? data : undefined,
+        params,
+    );
+    route.handle(req, fake, () => false);
+
+    if (send.mock.calls.length !== 0) {
+        console.warn(response);
+    }
+
+    expect(send)
+        .not
+        .toHaveBeenCalled();
+    expect(response.statusCode)
+        .toEqual(constants.HTTP_STATUS_BAD_REQUEST);
+    expect(JSON.stringify(response.body))
+        .toContain('missing');
+}
+
+export function testValidRoute(
+    route: GatewayInterfaceActionType,
+    data: any,
+    location: 'query' | 'body',
+    send: jest.Mock,
+    params?: any,
+) {
+    const response = new Response();
+    const fake = response as unknown as express.Response;
+    const req = request(
+        location === 'query' ? data : undefined,
+        location === 'body' ? data : undefined,
+        params,
+    );
+    route.handle(req, fake, () => false);
+
+    if (send.mock.calls.length !== 1) {
+        console.warn(response);
+    }
+
+    expect(send)
+        .toHaveBeenCalledTimes(1);
+    if (data !== undefined) {
+        expect(send.mock.calls[0][1])
+            .toEqual(expect.objectContaining(data));
+    }
+}
