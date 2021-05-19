89238729a8e326a0b9e163dad4833feedc2c4b1f
Ryan Delaney <vitineth@gmail.com>
2021-05-19 16:55:41 +0100

    Removing the current status check from the routes

--- src/attachments/system/SystemGatewayInterface.ts
+++ src/attachments/system/SystemGatewayInterface.ts
@@ -5,15 +5,7 @@ import { RequestHandler } from "express";
 export class SystemGatewayInterface implements GatewayAttachmentInterface {
 
     generateInterfaces(): GatewayMk2.GatewayInterfaceActionType[] | Promise<GatewayMk2.GatewayInterfaceActionType[]> {
-        const response = process.env.npm_package_version ?? '0.0.1';
         return [
-            {
-                action: 'get',
-                path: '/status',
-                handle: (req, res) => res.status(200)
-                    .send(`OK ${response}`),
-                secure: false,
-            },
             {
                 action: 'get',
                 path: '/whoami',
