3152c08353bfb9287c0862d8e2ffbb8f9c298257
Ryan Delaney <vitineth@gmail.com>
2021-12-22 15:31:45 +0000

    Adding an error code for deleting something failing due to dependents

--- src/constants/ErrorCodes.ts
+++ src/constants/ErrorCodes.ts
@@ -6,4 +6,5 @@ const generate = (code: string, message: string) => ({
 export const ErrorCodes = {
     FAILED: generate('FAILED', 'An unspecified error occurred in the service, please try again later'),
     SERVICE_TIMEOUT: generate('SERVICE_TIMEOUT', 'The service did not respond within the maximum allocated time, please try again later'),
+    DEPENDENTS: generate('DEPENDENTS', 'This entity cannot be deleted because other entities are currently depending on it'),
 };
