4b95e8d53123028ef40123cd2c126f9c8c4ee300
Ryan Delaney <vitineth@gmail.com>
2022-06-27 00:41:36 +0100

    Including the current roles the user has in /whoami
    This should allow the frontend to change up some features depending on permissions. Namely can show edit buttons and stuff around the UI depending on permissions

--- src/attachments/system/SystemGatewayInterface.ts
+++ src/attachments/system/SystemGatewayInterface.ts
@@ -1,8 +1,23 @@
 import { GatewayMk2 } from '../../Gateway';
 import GatewayAttachmentInterface = GatewayMk2.GatewayAttachmentInterface;
 import { RequestHandler } from "express";
+import utils from 'util';
 
 export class SystemGatewayInterface implements GatewayAttachmentInterface {
+
+    private static me(): RequestHandler {
+        return (req, res) => {
+            res.json({
+                username: req.uemsUser.username,
+                profile: req.uemsUser.profile,
+                name: req.uemsUser.fullName,
+                // TODO: is there a better way to do this?
+                isOps: req.kauth?.grant?.access_token?.hasRole?.('ops') ?? false,
+                isEnts: req.kauth?.grant?.access_token?.hasRole?.('ents') ?? false,
+                isAdmin: req.kauth?.grant?.access_token?.hasRole?.('admin') ?? false,
+            });
+        };
+    }
 
     generateInterfaces(): GatewayMk2.GatewayInterfaceActionType[] | Promise<GatewayMk2.GatewayInterfaceActionType[]> {
         return [
@@ -13,15 +28,5 @@ export class SystemGatewayInterface implements GatewayAttachmentInterface {
                 secure: [],
             },
         ];
-    }
-
-    private static me(): RequestHandler {
-        return (req, res) => {
-            res.json({
-                username: req.uemsUser.username,
-                profile: req.uemsUser.profile,
-                name: req.uemsUser.fullName,
-            });
-        };
     }
 }
